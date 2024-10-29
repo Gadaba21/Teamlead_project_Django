2b960cfb03c519231b450326db5f9067e28c5e09
Григорий Плаксин <plaksin28@mail.ru>
2024-10-29 20:55:18 +0300

    создал сюаризатор, заменил user временно, чтобы открывался редок

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 from .constanst import LENGTH_TEXT, MIN_VALUE, MAX_VALUE
-from user.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+User = get_user_model()
 
 class Category(models.Model):
     pass
